75b310ab7e57706f2e24e0209aeb54bb71f917dc
Tejora <numan.992@gmail.com>
2023-01-20 21:32:29 +0530

    Task commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task17_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetResult(string txtNum1, string txtNum2, string chkSelect)
+        {
+            int a = Convert.ToInt32(txtNum1);
+            int b = Convert.ToInt32(txtNum2);
+            if(chkSelect == "1")
+            {
+                ViewBag.Result = "Addition Result is " + (a + b).ToString();
+            }
+            else
+            {
+                ViewBag.Result = "Subtraction Result is " + (a - b).ToString();
+            }
+            return View("Index");
+        }
+
+        
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Task17_MVC.MvcApplication" Language="C#" %>
